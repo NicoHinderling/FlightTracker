1189fb4b59e3537654043049b6d2cbe0436596b5
NicoHinderling <nicolas.hinderling@gmail.com>
2015-08-08 23:31:05 -0700

    Should probably update this too :^)

--- env_variables_example.py
+++ env_variables_example.py
@@ -1,8 +1,14 @@
-#API Credentials
+# API Credentials
+QPX_API_KEY = ""
 
-QPX = ""
+# Environment Variables
 
-origin = ""
-destination = ""
-departure_date = ""
-return_date = ""
+# This is simply my configuration, so feel free to change this!
+ORIGIN_ONE = "BOS"
+DESTINATION_ONE = "LAX"
+
+ORIGIN_TWO = "NYC"
+DESTINATION_TWO = "MIA"
+
+DEPARTURE_DATE = "2015-12-19"
+# RETURN_DATE = "2015-11-29"

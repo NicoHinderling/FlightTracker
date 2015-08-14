c09f4524ad853b0a706e997ec75d5cfa36b30b9f
NicoHinderling <nicolas.hinderling@gmail.com>
2015-08-13 22:10:43 -0700

    Adding a second API key

--- worker.py
+++ worker.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from time import sleep
 from db_commands import FlightTrackerCluster
 from env_variables import QPX_API_KEY, ORIGIN_ONE, DESTINATION_ONE, ORIGIN_TWO, DESTINATION_TWO, DEPARTURE_DATE
+from env_variables import ORIGIN_THREE, DESTINATION_THREE, ORIGIN_FOUR, DESTINATION_FOUR
 # from env_variables import RETURN_DATE
 
 ############################################## Set-Up ################################################
@@ -23,45 +24,55 @@ if first == 'Y':
 else:
     print("\nStarting up the script! \n")
 
+
 params_one = myCluster.getParams(ORIGIN_ONE, DESTINATION_ONE, DEPARTURE_DATE, 100)
 params_two = myCluster.getParams(ORIGIN_TWO, DESTINATION_TWO, DEPARTURE_DATE, 100)
+params_three = myCluster.getParams(ORIGIN_THREE, DESTINATION_THREE, DEPARTURE_DATE, 100)
+params_four = myCluster.getParams(ORIGIN_FOUR, DESTINATION_FOUR, DEPARTURE_DATE, 100)
 # params = myCluster.getParams(ORIGIN_ONE, DESTINATION_ONE, DEPARTURE_DATE, RETURN_DATE, 100)
 
 last = None
 
-def writeToCassandra(params, time, day_of_week, origin, destination, departure_date):
-        url = 'https://www.googleapis.com/qpxExpress/v1/trips/search?key=' + QPX_API_KEY
-        headers = {'content-type': 'application/json'}
-        
-        response = requests.post(url, data=json.dumps(params), headers=headers)
-        data = response.json()
+def writeToCassandra(params, time, day_of_week, origin, destination, departure_date, api_key):
+    url = 'https://www.googleapis.com/qpxExpress/v1/trips/search?key=' + QPX_API_KEY
+    headers = {'content-type': 'application/json'}
+    
+    response = requests.post(url, data=json.dumps(params), headers=headers)
+    data = response.json()
 
-        price = data['trips']['tripOption'][0]['saleTotal']
-        carrier = data['trips']['tripOption'][0]['slice'][0]['segment'][0]['flight']['carrier']
+    price = data['trips']['tripOption'][0]['saleTotal']
+    carrier = data['trips']['tripOption'][0]['slice'][0]['segment'][0]['flight']['carrier']
 
-        time = time + origin
-        
-        concat = time + ", " +  day_of_week + ", " + price + ", " + carrier + ", " + origin + ", " + destination + ", " + departure_date
-        print concat + "    [" + concat + "]"
+    time = time + origin
+    
+    concat = time + ", " +  day_of_week + ", " + price + ", " + carrier + ", " + origin + ", " + destination + ", " + departure_date
+    print concat + "    [" + concat + "]"
 
-        myCluster.insertData('Tickets', ['time', 'day', 'price', 'carrier', 'origin', 'destination', 'departure_date', 'concat'], 
-            [time, day_of_week, price, carrier, origin, destination, departure_date, concat])
+    myCluster.insertData('Tickets', ['time', 'day', 'price', 'carrier', 'origin', 'destination', 'departure_date', 'concat'], 
+        [time, day_of_week, price, carrier, origin, destination, departure_date, concat])
 ############################################## Set-Up ################################################
 
 ############################################## SCRIPT ################################################
-
 while True:
-    if last != datetime.now().hour:
-        last = datetime.now().hour
-        print "DATA:   Hour-Day-Month-Year, Price, Carrier, Origin, Destination, Date, [Everything together]"
-        print "---------------------------------------------------------------------------------------------"
+    print "DATA:   Hour-Day-Month-Year, Price, Carrier, Origin, Destination, Date, [Everything together]"
+    print "---------------------------------------------------------------------------------------------"
 
-        time = str(datetime.now().hour) + "-" + str(datetime.now().day) + "-" + \
-            str(datetime.now().month) + "-" + str(datetime.now().year)
-        day_of_week = datetime.today().weekday()
-        which_day = days[day_of_week]
+    time = str(datetime.now().hour) + "-" + str(datetime.now().day) + "-" + \
+        str(datetime.now().month) + "-" + str(datetime.now().year)
+    day_of_week = datetime.today().weekday()
+    which_day = days[day_of_week]
 
-        writeToCassandra(params_one, time, which_day, ORIGIN_ONE, DESTINATION_ONE, DEPARTURE_DATE)
-        writeToCassandra(params_two, time, which_day, ORIGIN_TWO, DESTINATION_TWO, DEPARTURE_DATE)
-
+    writeToCassandra(params_one, time, which_day, ORIGIN_ONE, DESTINATION_ONE, DEPARTURE_DATE, QPX_API_KEY)
+    writeToCassandra(params_two, time, which_day, ORIGIN_TWO, DESTINATION_TWO, DEPARTURE_DATE, QPX_API_KEY)
+    writeToCassandra(params_three, time, which_day, ORIGIN_THREE, DESTINATION_THREE, DEPARTURE_DATE, QPX_API_KEY_TWO)
+    writeToCassandra(params_four, time, which_day, ORIGIN_FOUR, DESTINATION_FOUR, DEPARTURE_DATE, QPX_API_KEY_TWO)
+    sleep(3600)
 ############################################## SCRIPT ################################################
+
+
+
+
+
+
+
+
